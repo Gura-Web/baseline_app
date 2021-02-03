a093c2ff84a3e895d75ad267f3a16d2f012131a7
K-shir0 <kakishiro2190394@gmail.com>
2021-02-03 12:22:23 +0900

    fix: 余計な通信していたため削除

--- src/assets/script/Axios/axiosUser.js
+++ src/assets/script/Axios/axiosUser.js
@@ -1,117 +1,132 @@
-import {apiClient} from '../index';
-export const getMyData = (notLoginFunc) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get("/api/auth/user")
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            notLoginFunc();
-          }
-        });
+import { apiClient } from '../index';
+
+export const getMyData = notLoginFunc => {
+  return apiClient
+    .get('/api/auth/user')
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        notLoginFunc();
+      }
     });
-  };
-  
-export const editProfile = (user_id,postData) => {
+};
+
+export const editProfile = (user_id, postData) => {
   console.log(user_id, postData);
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/user/edit/${user_id}`,postData)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          console.log(response)
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
+  return apiClient
+    .post(`/api/user/edit/${user_id}`, postData)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      console.log(response);
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+      }
     });
-  };
+};
 
-export const deleteAccount = (user_id) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/user/delete/${user_id}`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          console.log(response)
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
-    });
-  };
+export const deleteAccount = user_id => {
+  return apiClient.get('/sanctum/csrf-cookie').then(response => {
+    return apiClient
+      .post(`/api/user/delete/${user_id}`)
+      .then(response => {
+        if (response.status !== 200) {
+          return false;
+        }
+        console.log(response);
+        return response;
+      })
+      .catch(error => {
+        console.error(error);
+        if (
+          error.response.status === 401 ||
+          error.response.status === 422 ||
+          error.response.status === 500
+        ) {
+        }
+      });
+  });
+};
 
-export const passwordResetMail = (postData) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/auth/password_reset_mail`,postData)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          console.log(response)
-          return true;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
-    });
-  };
-  
-export const passwordReset = (postData) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/auth/password_reset`,postData)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          console.log(response)
-          return true;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
-    });
-  };
+export const passwordResetMail = postData => {
+  return apiClient.get('/sanctum/csrf-cookie').then(response => {
+    return apiClient
+      .post(`/api/auth/password_reset_mail`, postData)
+      .then(response => {
+        if (response.status !== 200) {
+          return false;
+        }
+        console.log(response);
+        return true;
+      })
+      .catch(error => {
+        console.error(error);
+        if (
+          error.response.status === 401 ||
+          error.response.status === 422 ||
+          error.response.status === 500
+        ) {
+        }
+      });
+  });
+};
 
-export const passwordChange = (postData) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/auth/password_change`,postData)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          console.log(response)
-          return true;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
+export const passwordReset = postData => {
+  return apiClient
+    .post(`/api/auth/password_reset`, postData)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      console.log(response);
+      return true;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+      }
     });
-  };
-  
+};
 
+export const passwordChange = postData => {
+  return apiClient
+    .post(`/api/auth/password_change`, postData)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      console.log(response);
+      return true;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+      }
+    });
+};
