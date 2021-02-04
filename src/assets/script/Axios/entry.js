df2da3c0373768d4818cb4ddcca4e3b56dfd7660
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 11:29:37 +0900

    entry.jsの変更

--- src/assets/script/Axios/entry.js
+++ src/assets/script/Axios/entry.js
@@ -1,42 +1,45 @@
-import {apiClient} from '../index';
-export const indexEntry = (id) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/entry/show/${id}`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+import { apiClient } from '../index';
+export const indexEntry = id => {
+  return apiClient
+    .get(`/api/entry/show/${id}`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
+};
 
-  export const registEntry = (postData) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/entry`, postData)
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            // alert("成功");
-            return true;
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("失敗");
-            return true;
-          }
-          return true;
-        });
+export const registEntry = postData => {
+  return apiClient
+    .post(`/api/entry`, postData)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // alert("成功");
+        return true;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('失敗');
+        return true;
+      }
+      return true;
     });
-  };
-  
+};
