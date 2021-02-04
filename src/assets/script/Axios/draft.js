e4a5fca9504e8a19fdf067984d77f598fedf2d48
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 11:28:13 +0900

    draft.jsの変更

--- src/assets/script/Axios/draft.js
+++ src/assets/script/Axios/draft.js
@@ -1,57 +1,62 @@
-import {apiClient} from '../index';
+import { apiClient } from '../index';
 export const indexDraft = () => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/draft`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+  return apiClient
+    .get(`/api/draft`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
-  
-  export const registDraft = (postData) => {
-    apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      apiClient
-        .post(`/api/draft`, postData)
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            // alert("成功");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("失敗");
-          }
-        });
+};
+
+export const registDraft = postData => {
+  apiClient
+    .post(`/api/draft`, postData)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // alert("成功");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('失敗');
+      }
     });
-  };
-  export const deleteDraft = (id) => {
-    apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      apiClient
-        .post(`/api/draft/delete/${id}`)
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            // alert("成功");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("失敗");
-          }
-        });
+};
+export const deleteDraft = id => {
+  apiClient
+    .post(`/api/draft/delete/${id}`)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // alert("成功");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('失敗');
+      }
     });
-  };
-  
+};
