fa8185c5e371644196c246c7fe4866dcc1ab5fbe
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 11:32:29 +0900

    getHomeData.jsの変更

--- src/assets/script/Axios/getHomeData.js
+++ src/assets/script/Axios/getHomeData.js
@@ -1,24 +1,28 @@
-import {apiClient} from '../index';
-export const getHomeData = (notLoginFunc) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get("/api/home")
-        .then((response) => {
-          if (response.status !== 200) {
-            notLoginFunc();
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            // alert("データ取得失敗")
-            console.log("データ取得失敗")
-            notLoginFunc();
-          }
-        });
-    }).catch(()=>{
-      notLoginFunc();
-    }) 
-  };
+import { apiClient } from '../index';
+export const getHomeData = notLoginFunc => {
+  // return apiClient.get("/sanctum/csrf-cookie").then((response) => {
+  return apiClient
+    .get('/api/home')
+    .then(response => {
+      if (response.status !== 200) {
+        notLoginFunc();
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        // alert("データ取得失敗")
+        console.log('データ取得失敗');
+        notLoginFunc();
+      }
+    });
+  // }).catch(()=>{
+  //   notLoginFunc();
+  // })
+};
