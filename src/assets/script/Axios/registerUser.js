1846863e08d171b80df05d5f9b710adb788e0026
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 12:03:28 +0900

    registerUser.jsの変更

--- src/assets/script/Axios/registerUser.js
+++ src/assets/script/Axios/registerUser.js
@@ -1,43 +1,43 @@
-import { apiClient } from "../index";
-export const temporaryRegistationUser = (email) => {
-  return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-    return apiClient
-      .post("/api/user/temporary_register/1", {email: email})
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          return true;
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        if (error.response.status === 401 || error.response.status === 500) {
-          alert("ログイン失敗");
-          return false;
-        }
-        if(error.response.status === 422) {
-          alert('そのメールアドレスは既に登録されています');
-          return false;
-        }
-      });
-  });
-};
-export const registerUser = (postData) => {
-  return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-    return apiClient
-      .post("/api/user/register", postData)
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          return true;
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          return false;
-        }
-      });
-  });
+import { apiClient } from '../index';
+export const temporaryRegistationUser = email => {
+  return apiClient
+    .post('/api/user/temporary_register/1', { email: email })
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        return true;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (error.response.status === 401 || error.response.status === 500) {
+        alert('ログイン失敗');
+        return false;
+      }
+      if (error.response.status === 422) {
+        alert('そのメールアドレスは既に登録されています');
+        return false;
+      }
+    });
 };
 
+export const registerUser = postData => {
+  return apiClient
+    .post('/api/user/register', postData)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        return true;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        return false;
+      }
+    });
+};
