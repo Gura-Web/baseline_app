cc66080762722857463836b900722b9db95beede
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 12:14:58 +0900

    selectItems.jsの変更

--- src/assets/script/Axios/selectItems.js
+++ src/assets/script/Axios/selectItems.js
@@ -1,56 +1,62 @@
-import {apiClient} from '../index';
+import { apiClient } from '../index';
 export const indexJob = () => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/occupational_category`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+  return apiClient
+    .get(`/api/occupational_category`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
+};
 export const indexInternship = () => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/internship`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+  return apiClient
+    .get(`/api/internship`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
+};
 
 export const indexYearGraduation = () => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/year_of_graduation`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+  return apiClient
+    .get(`/api/year_of_graduation`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
+};
