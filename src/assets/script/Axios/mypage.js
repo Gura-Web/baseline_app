67752010431fbfb6e886caa97bca562dc2465aa7
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 11:51:43 +0900

    mypage.jsの変更

--- src/assets/script/Axios/mypage.js
+++ src/assets/script/Axios/mypage.js
@@ -1,36 +1,39 @@
-import {apiClient} from '../index';
+import { apiClient } from '../index';
 export const mypage = () => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/mypage`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
+  return apiClient
+    .get(`/api/mypage`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+      }
     });
-  };
-export const getUserData = (id) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .get(`/api/mypage/${id}`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-          }
-        });
+};
+export const getUserData = id => {
+  return apiClient
+    .get(`/api/mypage/${id}`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+      }
     });
-  };
-
+};
