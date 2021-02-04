ea39e566ae9ecf8e64d8f911217dc6fcdd39fe7d
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 11:41:33 +0900

    interview copy.jsの変更

--- src/assets/script/Axios/interview copy.js
+++ src/assets/script/Axios/interview copy.js
@@ -1,20 +1,21 @@
-import {apiClient} from '../index';
-  export const registInterview = (postData) => {
-    apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      apiClient
-        .post(`/api/interview`, postData)
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            // alert("成功");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("失敗");
-          }
-        });
+import { apiClient } from '../index';
+export const registInterview = postData => {
+  apiClient
+    .post(`/api/interview`, postData)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // alert("成功");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('失敗');
+      }
     });
-  };
-  
+};
