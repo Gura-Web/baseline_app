8cf4268b470d950adab09673cc1e23f64f0df18b
Ryota-nino <ryota.swimmer@icloud.com>
2021-02-04 12:09:06 +0900

    selection.jsの変更

--- src/assets/script/Axios/selection.js
+++ src/assets/script/Axios/selection.js
@@ -1,62 +1,67 @@
-import {apiClient} from '../index';
+import { apiClient } from '../index';
 
-  export const registSelection = (postData) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-        .post(`/api/selection`, postData)
-        .then((response) => {
-          console.log(response);
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("記入されていない項目があります");
-            return false;
-          }
-          return false;
-        });
+export const registSelection = postData => {
+  return apiClient
+    .post(`/api/selection`, postData)
+    .then(response => {
+      console.log(response);
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('記入されていない項目があります');
+        return false;
+      }
+      return false;
     });
-  };
+};
 
-  export const editSelection = (id,postData) => {
-    apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      apiClient
-        .post(`/api/interview/edit/${id}`, postData)
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
-            // alert("成功");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("失敗");
-          }
-        });
+export const editSelection = (id, postData) => {
+  apiClient
+    .post(`/api/interview/edit/${id}`, postData)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        // alert("成功");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('失敗');
+      }
     });
-  };
+};
 
-  export const showSelection = (id) => {
-    return apiClient.get("/sanctum/csrf-cookie").then((response) => {
-      return apiClient
-      .get(`/api/selection/show/${id}`)
-        .then((response) => {
-          if (response.status !== 200) {
-            return false;
-          }
-          return response;
-        })
-        .catch((error) => {
-          console.error(error);
-          if (error.response.status === 401 || error.response.status === 422 || error.response.status === 500) {
-            alert("データ取得失敗")
-          }
-        });
+export const showSelection = id => {
+  return apiClient
+    .get(`/api/selection/show/${id}`)
+    .then(response => {
+      if (response.status !== 200) {
+        return false;
+      }
+      return response;
+    })
+    .catch(error => {
+      console.error(error);
+      if (
+        error.response.status === 401 ||
+        error.response.status === 422 ||
+        error.response.status === 500
+      ) {
+        alert('データ取得失敗');
+      }
     });
-  };
-  
+};
