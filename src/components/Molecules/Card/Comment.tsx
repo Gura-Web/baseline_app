5a5eaa2c93654a0c0e9c51da7c33937ca287bbbe
K-shir0 <kakishiro2190394@gmail.com>
2021-02-06 12:23:59 +0900

    feat: 新しいコメントの雛形が完成

--- src/components/Molecules/Card/Comment.tsx
+++ src/components/Molecules/Card/Comment.tsx
@@ -1,42 +1,45 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { pageTransitionNormal } from '../../../assets/script/pageTransition';
-import {
-  ArrowIcon,
-  PencilIcon,
-  TrashIcon,
-  rikuma,
-} from '../../../assets/images/index';
 import { motion } from 'framer-motion';
+import { pageTransitionNormal } from '../../../assets/script';
+import { ArrowIcon, rikuma } from '../../../assets/images/index';
+import { User } from '../../../services/models';
+
 interface Props {
-  name: string;
-  year: string;
-  txt: string;
+  // 表示名
+  author: User;
+  postedYear: string;
+  contents: string;
   updateTime: string;
   isArrow: boolean;
-  id?: number;
-  type?: 'user' | 'mypage';
-  clickFunc?: any;
-  clickFunc2?: any;
-  icon?: string;
+  menu: JSX.Element;
 }
 
-const OldComment: React.FC<Props> = props => {
+export const Comment: React.FC<Props> = ({
+  author,
+  isArrow = false,
+  contents,
+  updateTime,
+  postedYear,
+  menu,
+}) => {
+  const { firstName, lastName, iconImageUrl } = author;
+
   const activityTxtEl = useRef<HTMLParagraphElement>(null);
 
   const setUrlText = (): void => {
     const reg = /((https?|ftp)(:\/\/[-_.!~*'()a-zA-Z0-9;/?:@&=+$,%#]+))/;
-    const txtEl: string = activityTxtEl.current!.innerText;
-    const searchUrlTxt = txtEl.match(reg);
-    if (searchUrlTxt) {
+    const txtEl = activityTxtEl.current?.innerText;
+    const searchUrlTxt = txtEl?.match(reg);
+    if (searchUrlTxt && txtEl) {
       activityTxtEl.current!.innerHTML = txtEl.replace(
         reg,
-        `<a href="${searchUrlTxt![0]}">${searchUrlTxt![0]}</a>`,
+        `<a href="${searchUrlTxt[0]}">${searchUrlTxt[0]}</a>`,
       );
     }
   };
 
   const timeTextConversion = () => {
-    const dateTime: string = String(props.updateTime).slice(0, 10);
+    const dateTime: string = String(updateTime).slice(0, 10);
     const timeText: string = dateTime.replace(/-/g, '.');
     const texts: {
       dateTime: string;
@@ -45,25 +48,17 @@ const OldComment: React.FC<Props> = props => {
       dateTime,
       timeText,
     };
+
     return texts;
   };
+
   useEffect((): void => {
     setUrlText();
   }, []);
 
-  let [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
   const arrowClickHandler = (): void => {
     setToggleMenu(!toggleMenu);
-  };
-
-  const isArrowRender = () => {
-    if (props.isArrow) {
-      return (
-        <div className={`activity-item__arrow`} onClick={arrowClickHandler}>
-          <img src={ArrowIcon} alt="" />
-        </div>
-      );
-    }
   };
 
   return (
@@ -74,12 +69,15 @@ const OldComment: React.FC<Props> = props => {
       exit="out"
       variants={pageTransitionNormal}
     >
-      <img className="userIcon" src={props.icon ? props.icon : rikuma} alt="" />
+      <img className="userIcon" src={iconImageUrl || rikuma} alt="" />
       <div className="activity-item__content">
         <div className="activity-item__head">
-          <h1 className="activity-item__name">{props.name}</h1>
+          {/* 名前部分 */}
+          <h1 className="activity-item__name">
+            {firstName} {lastName}
+          </h1>
           <ul className="activity-item__list">
-            <li>{props.year}年次</li>
+            <li>{postedYear}年次</li>
             <li>&nbsp;|&nbsp;</li>
             <li>
               <time dateTime={timeTextConversion().dateTime}>
@@ -87,33 +85,33 @@ const OldComment: React.FC<Props> = props => {
               </time>
             </li>
           </ul>
-          {isArrowRender()}
-          {/* {props.type === "mypage" && isArrowRender()} */}
+          {isArrow && (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+            <div className="activity-item__arrow" onClick={arrowClickHandler}>
+              <img src={ArrowIcon} alt="" />
+            </div>
+          )}
         </div>
         <p ref={activityTxtEl} className="activity-item__txt">
-          {props.txt}
+          {contents}
         </p>
       </div>
-      {/* {props.type === "mypage" && ( */}
-      <ul className={`activity-item-menu ${toggleMenu && 'view'}`}>
-        <li
-          className="activity-item-menu__item"
-          onClick={() => props.clickFunc(props.id, true, props.txt)}
-        >
-          <img src={PencilIcon} alt="" />
-          <span onClick={() => setToggleMenu(false)}>編集</span>
-        </li>
-        <li
-          className="activity-item-menu__item cAttention"
-          onClick={() => props.clickFunc2(props.id, true)}
-        >
-          <img src={TrashIcon} alt="" />
-          <span onClick={() => setToggleMenu(false)}>削除</span>
-        </li>
+      {/* TODO 改修 */}
+
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
+      <ul
+        className={`activity-item-menu ${toggleMenu && 'view'}`}
+        onClick={() => {
+          if (toggleMenu) {
+            setToggleMenu(false);
+          }
+        }}
+      >
+        {menu}
+        {/* 編集ボタン */}
       </ul>
+      {/* TODO 改修 */}
       {/* )} */}
     </motion.article>
   );
 };
-
-export default OldComment;
