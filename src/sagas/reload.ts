3619a0d8bd518cb1644245e65e0d91e0edd9f0d2
K-shir0 <kakishiro2190394@gmail.com>
2021-02-10 04:20:06 +0900

    feat: リロードする処理を切り分けた

--- src/sagas/reload.ts
+++ src/sagas/reload.ts
@@ -1,14 +1,19 @@
-import { take, select, takeLatest, fork } from 'redux-saga/effects';
-import { reload } from '../actions/reload/reload';
+import { select, takeLatest, fork, put } from 'redux-saga/effects';
+import { reload, SetReloadParams } from '../actions/reload/reload';
 
 import * as Action from '../actions/reload/actions';
 
-function* runReload(action: ReturnType<typeof reload.reloadStart>) {
+function* runReload() {
   const state = yield select();
-  console.log(state.reload);
-  // try
-  //
-  // }
+  const reloadState: SetReloadParams = state.reload;
+
+  try {
+    yield* reloadState.reloadHandlers.map(reloadHandle => reloadHandle());
+
+    yield put(reload.reloadSucceed());
+  } catch (error) {
+    yield put(reload.reloadFailed(error));
+  }
 }
 
 export function* watchReload() {
